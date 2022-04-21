ead881b5d20f382e34f35f9483bf7cb18fb9543c
ray <ryanbalota@gmail.com>
2022-04-21 12:11:45 -0400

    gggggg

--- msystem/models.py
+++ msystem/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
-
-class List(models.Model):
-    pass
-
-
-class Item(models.Model):
-    npet = models.TextField(default='')
-    nname = models.TextField(default='')
-    nAddress = models.TextField(default='')
-    nBreed = models.TextField(default='')
-    nDay = models.TextField(default='') 
-    list = models.ForeignKey(List, default=None, on_delete=models.PROTECT)
-
+# Create your models here.
--- msystem/views.py
+++ msystem/views.py
@@ -1,25 +1,6 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from msystem.models import Item, List
+from django.shortcuts import render
+from django.http import  HttpResponse
 
-
-def home_page(request):
-    items = Item.objects.all()
-    return render(request, 'homepage.html',{'items' : items})
-    
-
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'mainpage.html', {'list': list_})
-
-
-def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(npet=request.POST['pet'],nname =request.POST['owner'],nAddress=request.POST['address'],nBreed =request.POST['breed'],nDay =request.POST['birthday'], list=list_)
-    return redirect(f'/Msystem/{list_.id}/')
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(npet=request.POST['pet'],nBreed =request.POST['breed'],nDay =request.POST['birthday'],list=list_)
-    return redirect(f'/Msystem/{list_.id}/')
+def MainPage(request):
+	return HttpResponse('<html><title>4ps Monitoring System</title></html>')
+# Create your views here.
