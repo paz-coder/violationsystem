b64790d233221a2f3389b33d5b6673e82db44d5e
ray <ryanbalota@gmail.com>
2022-04-21 12:07:43 -0400

    gggggg

--- msystem/views.py
+++ msystem/views.py
@@ -11,7 +11,7 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'registration.html', {'list': list_})
+    return render(request, 'mainpage.html', {'list': list_})
 
 
 def new_list(request):
