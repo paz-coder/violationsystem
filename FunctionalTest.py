1e5474c05062d564826015d7089ebeea38ff1e6d
ray <ryanbalota@gmail.com>
2022-04-21 11:34:31 -0400

    gggggg

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-class HomePageTest(TestCase):
+class HomePageTest(unittest.TestCase):
 	def wait_for_table(self, row_text):        
            start_time = time.time()
            while True:  
