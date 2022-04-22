60c8338d4357e21549fb3266e950a62b8215655f
ray <ryanbalota@gmail.com>
2022-04-21 22:31:02 -0400

    gggggg

--- msystem/views.py
+++ msystem/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 def MainPage(request):
 	# if request.method == 'POST':
 	# 	return HttpResponse (request.POST['attribute'])
-	return render(request, 'mainpage.html', {'NewName': request.POST.get('attribute'),})
+	return render(request, 'mainpage.html', {'NewName': request.POST.get('newname'),})
 def New_list(request):
 	# if request.method == 'POST':
 	# 	return HttpResponse (request.POST['attribute'])
