7162f1b3191f4ddb981ac50a6934117f4efbc503
ray <ryanbalota@gmail.com>
2022-04-22 00:05:09 -0400

    gggggg

--- msystem/views.py
+++ msystem/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def MainPage(request):
-	return render(request, 'mainpage.html', {'Name': request.POST.get('Name'), 'address': request.POST.get('Address1'), 'age': request.POST.get('Age1'), 'dswd': request.POST.get('Dswd1')})
+	return render(request, 'mainpage.html', {'Name': request.POST.get('NAME'), 'address': request.POST.get('ADDRESS'), 'age': request.POST.get('AGE'), 'dswd': request.POST.get('DSWD')})
