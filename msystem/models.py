06d52efd8297086b33c7a9064b3a9b73cc1b282c
ray <ryanbalota@gmail.com>
2022-05-13 01:39:49 -0400

    Gggggg

--- msystem/models.py
+++ msystem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+    
+class List(models.Model):
+    pass
+
+class Item(models.Model):    
+    Name = models.TextField(default='')
+    Address = models.TextField(default='')  
+    Age = models.TextField(default='')
+    Dswd = models.TextField(default='')
+
+
+
+
+    list = models.ForeignKey(List, default=None, on_delete=models.PROTECT)
--- msystem/tests.py
+++ msystem/tests.py
@@ -21,12 +21,35 @@ class HomePageTest(TestCase):
 		self.assertTemplateUsed(resp, 'mainpage.html')
 		
 		resp = self.client.post('/', data={'dswd' :'Dswd'})
-		self.assertIn('Dswd', resp.content.decode())
+		self.assertIn('dswd', resp.content.decode())
 		self.assertTemplateUsed(resp, 'mainpage.html')
 
 		resp = self.client.post('/', data={'member' :'Member'})
 		self.assertIn('Member', resp.content.decode())
 		self.assertTemplateUsed(resp, 'mainpage.html')
+
+class ORM(TestCase):
+
+   def test_saving_and_retrieving_items(self):
+     
+      entry1 = Users() 
+      entry1.Name = 'Ray'      
+      entry1.addres = 'brgy.'
+      entry1.age = '22.'
+      entry1.dswd = '00111'
+      entry1.save()   
+      entry1 = Item()  
+      entry2.Name = 'Ryan'      
+      entry2.addres = 'Block o'
+      entry2.age = '23'
+      entry2.dswd = '5555'
+      entry2.save()      
+      items = item.object.all()                  
+      self.assertEqual(items.count(), 2) 
+      items1 = items[0]
+      items2 = items[2]      	     
+      self.assertEqual(items1.brgyid, '143')
+      self.assertEqual(items2.brgyid, '134')
 
 
 
