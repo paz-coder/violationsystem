8751fa9355dacfd80cd3e6bcc5eb7560fa21d2e0
hazelbarcibal <hazelannmae.barcibal@gsfe.tupcavite.edu.ph>
2022-07-26 22:03:00 +0800

    dashboard sourcecode

--- djangoproj/djangoproj/settings.py
+++ djangoproj/djangoproj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventorysystem.apps.InventorysystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- djangoproj/inventorysystem/views.py
+++ djangoproj/inventorysystem/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('<h2>Supply Inventory System<h2> <h3>lets get it !!!!!<h3>')
+    return render(request, 'task/home.html')
