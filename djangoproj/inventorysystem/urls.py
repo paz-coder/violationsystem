b691494f78f3dc364ce9f1b5d6fb0050485d44f1
paz-coder <eldzpaz16@gmail.com>
2022-07-29 18:25:00 +0800

    yey

--- djangoproj/inventorysystem/urls.py
+++ djangoproj/inventorysystem/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='inventorysystem-home'),
+    path('', views.home, name='inventorysystem_home'),
+    path('login/', views.login, name='inventorysystem_login'),
+    path('esupplydelivery/', views.esupplydelivery, name='inventorysystem_esupplydelivery'),
+    path('nsupplydelivery/', views.nsupplydelivery, name='inventorysystem_nsupplydelivery'),
+    path('supplyrequest/', views.supplyrequest, name='inventorysystem_supplyrequest'),
+    path('supplywithdraw/', views.supplywithdraw, name='inventorysystem_supplywithdraw'),
+    path('statuslimit/', views.statuslimit, name='inventorysystem_statuslimit'),
+    path('eequipedelivery/', views.eequipdelivery, name='inventorysystem_eequipdelivery'),
+    path('nequipedelivery/', views.nequipdelivery, name='inventorysystem_nequipdelivery'),
+    path('equiprequest/', views.equiprequest, name='inventorysystem_equiprequest'),
+    path('equipwithdraw/', views.equipwithdraw, name='inventorysystem_equipwithdraw'), 
+    path('equipreturn/', views.equipreturn, name='inventorysystem_equipreturn'), 
+    path('storagemapping/', views.storagemapping, name='inventorysystem_storagemapping'),      
 ]
--- djangoproj/inventorysystem/views.py
+++ djangoproj/inventorysystem/views.py
@@ -3,3 +3,39 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'task/home.html')
+
+def login(request):
+    return render(request, 'task/login.html')
+
+def esupplydelivery(request):
+    return render(request, 'task/esupplydelivery.html')
+
+def nsupplydelivery(request):
+    return render(request, 'task/nsupplydelivery.html')
+
+def supplyrequest(request):
+    return render(request, 'task/supplyrequest.html')
+
+def supplywithdraw(request):
+    return render(request, 'task/supplywithdraw.html')
+
+def statuslimit(request):
+    return render(request, 'task/statuslimit.html')
+
+def eequipdelivery(request):
+    return render(request, 'task/eequipdelivery.html')
+
+def nequipdelivery(request):
+    return render(request, 'task/nequipdelivery.html')
+
+def equiprequest(request):
+    return render(request, 'task/equiprequest.html')
+
+def equipwithdraw(request):
+    return render(request, 'task/equipwithdraw.html')
+
+def equipreturn(request):
+    return render(request, 'task/equipreturn.html')
+
+def storagemapping(request):
+    return render(request, 'task/storagemapping.html')
